e1459ce9a5f189509b986510a28be4cbcb8fd0c1
hojun <hojunjang12032gmail.com>
2024-01-20 11:40:53 +0900

    난이도 설정 & 게임 횟수 추가

--- src/1_find_number.py
+++ src/1_find_number.py
@@ -1,26 +1,28 @@
 import random
 random_value = random.randrange(1, 51)
 print(f"랜덤숫자: {random_value}")
+count = 0
+player = input("난이도를 설정하시오(상 중 하): ")
+if player == "하":
+  game_count = 10
+elif player == "중":
+  game_count = 5
+elif player == "상":
+  game_count = 3
 
-for i in range(3):
- input_value = input("숫자를 입력해주세요: ")
- if random_value == int(input_value):
-  print("정답입니다")
- elif random_value > int(input_value):
-  print("사용자의 값이 더 작습니다")
- elif random_value < int(input_value): 
-  print("사용자의 값이 더 큽니다")
- else:
-  print("정답이 아니다")
+count2 = game_count
 
- 
-
-
-
-#random.choice(>)
-#print("더 크다")ㄴ
-#random.choice(<)
-#input("더 작다")
-# random.choice([random_value])
-# True
-# print("정답이다")
+for i in range(game_count):
+  count = count + 1
+  count2 = count2 - 1
+  input_value = input("숫자를 입력해주세요: ")
+  if random_value == int(input_value):
+    print(f"{count}번만에 정답입니다")
+    break
+    
+  elif random_value > int(input_value):
+    print(f"{count2}번 남았고 사용자의 값이 더 작습니다")
+  elif random_value < int(input_value): 
+    print(f"{count2}번 남았고사용자의 값이 더 큽니다")
+  else:
+    print("정답이 아니다")
